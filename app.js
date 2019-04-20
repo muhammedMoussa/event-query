50f187ceba201382951e865e7abefe5a93693edc
Muhammed Moussa <muhammedmoussa@hotmail.com>
2019-04-20 16:31:56 +0200

    initial commit 🎉

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res, next) => {
+    res.send('Hello Event Query Word!')
+})
+app.listen(3000);
